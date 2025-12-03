a8c91ca60dbaf4895d74ad48ef358c6cb342290f
Steve Buer <steve@jxqz.org>
2025-12-03 13:12:30 -0800

    add pydeck notebook

--- homework5/cron-alerts/alert-10fm.sh
+++ homework5/cron-alerts/alert-10fm.sh
@@ -2,9 +2,10 @@
 
 # crontab script fpr DX alerts
 
-ALERTS=$(psql -f 10m-fm.sql --csv | grep -v time)
+ALERTS=$(psql -d dx_analysis -f /home/steve/cron-alerts/10m-fm.sql --csv | grep -v time)
 
 if [ ! -z $ALERTS ]; then
 
 	echo $ALERTS | mail -s "10m FM Alert" steve@jxqz.org
+	/home/steve/cron-alerts/voipms_sms.py 3609306443 2064576680 "$ALERTS"
 fi
